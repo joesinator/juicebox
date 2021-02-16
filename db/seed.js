af226cd7360efd9caef7e4f394fa910900f83d9e
joesinator <joeyread888@gmail.com>
2021-02-15 19:54:09 -0800

    working on updateUser

--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,16 @@
 const {client,
     getAllUsers,
-    createUser
+    createUser,
+    updateUser
 } = require('./index');
 
 async function createInitialUsers(){
     try{
         console.log("Starting to create users...");
-        const albert = await createUser({username: 'albert', password: 'bertie99'});
+        const albert = await createUser({username: 'albert', password: 'bertie99', name: 'Al Bert', location: 'Sidney, Australia'});
         console.log(albert);
-        const sandra = await createUser({username: 'sandra', password: '2sandy4me'});
-        const glamgal = await createUser({username: 'glamgal', password: 'soglam'});
+        const sandra = await createUser({username: 'sandra', password: '2sandy4me', name: 'Just Sandra', location: "Ain't tellin'"});
+        const glamgal = await createUser({username: 'glamgal', password: 'soglam', name: 'Joshua', location: 'East Upper Side'});
         console.log("Finished creating users!");
     } catch (error) {
         console.error("Error creating users!");
@@ -37,7 +38,10 @@ async function createTables(){
         CREATE TABLE users (
             id SERIAL PRIMARY KEY,
             username varchar(255) UNIQUE NOT NULL,
-            password varchar(255) NOT NULL
+            password varchar(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            location VARCHAR(255) NOT NULL,
+            active BOOLEAN DEFAULT true
         );
         `);
         console.log("Finished building tables!");
@@ -63,7 +67,13 @@ async function testDB(){
     try{
         console.log("Starting to test database...");
         const users = await getAllUsers();
-        console.log("getAllUsers:", users);
+        console.log("Result:", users);
+        console.log("Calling updateUser on users[0]");
+        const updateUserResult = await updateUser(users[0].id, {
+            name: "Newname Sogood",
+            location: "lesterville, KY"
+        });
+        console.log("Result:", updateUserResult)
         console.log("Finished database tests!");
     } catch (error) {
         console.error("Error testing database!");
